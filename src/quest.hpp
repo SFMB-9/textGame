4f8e5e876b67b4f3f9d9dbb01d49ca5d715b9e66
Salvador Federico Milanes Braniff <A01029956@ITESM.mx>
2022-09-08 12:58:19 -0500

    Added json constructor

--- src/quest.hpp
+++ src/quest.hpp
@@ -6,10 +6,20 @@
 #include <vector>
 
 #include "flag.hpp"
+#include "json.hpp"
+
+using Json = nlohmann::json;
 
 class Quest {
     public:
         Quest(std::string name, std::string desc);
+        Quest(Json json) {
+            name = json["name"];
+            desc = json["desc"];
+            for (auto flag : json["flags"]) {
+                flags.push_back(Flag(flag));
+            }
+        }
         std::string getName();
         std::string getDescription();
         void setName(std::string name);
