7d70a0731da30b23a01259522d4d93e37b721c57
Salvador Federico Milanes Braniff <A01029956@ITESM.mx>
2022-12-04 19:00:32 -0600

    Quest constructor

--- src/quest.hpp
+++ src/quest.hpp
@@ -14,21 +14,29 @@ class Quest {
     public:
         Quest(std::string name, std::string desc);
         Quest(Json json) {
-            name = json["name"];
-            desc = json["desc"];
-            for (auto flag : json["flags"]) {
-                flags.push_back(Flag(flag));
-            }
+            std::cout << "Quest constructor called" << std::endl;
+            std::cout << json << std::endl;
+            name = (json.find("name") != json.end()) ? json["name"] : "Untitled Quest";
+            desc = (json.find("description") != json.end()) ? json["description"] : "No description given";
+            // for (auto flag : json["flags"]) {
+            //     flags.push_back(Flag(flag));
+            // }
         }
-        std::string getName();
-        std::string getDescription();
+        std::string getName() { return name; }
+        std::string getDescription() { return desc; }
         void setName(std::string name);
         void setDescription(std::string desc);
-        void addFlag(Flag flag);
-        void removeFlag(Flag flag);
+        // void addFlag(Flag flag);
+        // void removeFlag(Flag flag);
         std::vector<Flag> getFlags();
         void setComplete();
         void setFail();
+
+        void printForQuestLog(){
+            std::cout << "========================" << std::endl;
+            std::cout << "||" << name << std::endl << "||" << desc << std::endl;
+            std::cout << "========================" << std::endl;
+        }
 
     private:
         bool fail;
