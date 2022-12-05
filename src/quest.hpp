5fa237fd266627ed628cbf1f78af3725dcafa8a0
Salvador Federico Milanes Braniff <A01029956@ITESM.mx>
2022-12-05 16:46:37 -0600

    Quest constructor

--- src/quest.hpp
+++ src/quest.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 
+#include "event.hpp"
 #include "flag.hpp"
 #include "json.hpp"
 
@@ -13,36 +14,34 @@ using Json = nlohmann::json;
 class Quest {
     public:
         Quest(std::string name, std::string desc);
-        Quest(Json json) {
+        Quest(Slot slot, Json json) {
             std::cout << "Quest constructor called" << std::endl;
-            std::cout << json << std::endl;
-            name = (json.find("name") != json.end()) ? json["name"] : "Untitled Quest";
-            desc = (json.find("description") != json.end()) ? json["description"] : "No description given";
-            // for (auto flag : json["flags"]) {
-            //     flags.push_back(Flag(flag));
+            // std::cout << json << std::endl;
+            // name = (json.find("name") != json.end()) ? json["name"] : "Untitled Quest";
+            // desc = (json.find("description") != json.end()) ? json["description"] : "No description given";
+            // for (auto event : json["events"]) {
+            //     events.push_back(Event(event, slot));
             // }
         }
-        std::string getName() { return name; }
-        std::string getDescription() { return desc; }
-        void setName(std::string name);
-        void setDescription(std::string desc);
-        // void addFlag(Flag flag);
-        // void removeFlag(Flag flag);
-        std::vector<Flag> getFlags();
-        void setComplete();
-        void setFail();
-
         void printForQuestLog(){
             std::cout << "========================" << std::endl;
             std::cout << "||" << name << std::endl << "||" << desc << std::endl;
             std::cout << "========================" << std::endl;
         }
+        void run(){
+            //Iterate through events and execute them
+            for (auto event : events) {
+                event.run();
+            }
+        }
 
     private:
+        Slot slot;
         bool fail;
         bool isComplete();
         std::string name;
         std::string desc;
+        std::vector<Event> events;
         std::vector<Flag> flags;
 };
 
